af2483d655f2e444a0daa675e012ee4abb9c83f9
Thányla Sales <thanylas@gmail.com>
2020-05-06 11:28:05 -0300

    Ciação classe ViaCepService

---
+++ cep-service/src/main/java/com/thanyla/cepservice/ViaCepService.java
@@ -0,0 +1,15 @@
+package com.thanyla.cepservice;
+
+
+public class ViaCepService extends AbstractCepService {
+
+    public ViaCepService(String dominio) {
+        super(dominio);
+    }
+
+    @Override
+    protected String buildPath(String cep) {
+        return String.format("ws/%s/json", cep);
+    }
+    
+}
